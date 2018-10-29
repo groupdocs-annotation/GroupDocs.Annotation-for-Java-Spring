2f26ab03a115337696e6422b01d2978283de74aa
Elena Kolesnikova <koleslena@yandex.ru>
2018-10-29 14:43:41 +0200

    made url parameter optional in updateDocument

--- src/main/java/com/groupdocs/ui/annotation/controller/AnnotationController.java
+++ src/main/java/com/groupdocs/ui/annotation/controller/AnnotationController.java
@@ -23,6 +23,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Nullable;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedInputStream;
@@ -141,8 +142,8 @@ public class AnnotationController {
      */
     @RequestMapping(value = "/uploadDocument", method = RequestMethod.POST, produces = APPLICATION_JSON_VALUE, consumes = MULTIPART_FORM_DATA_VALUE)
     @ResponseBody
-    public UploadedDocumentEntity uploadDocument(@RequestParam("file") MultipartFile content,
-                                                 @RequestParam("url") String url,
+    public UploadedDocumentEntity uploadDocument(@Nullable @RequestParam("file") MultipartFile content,
+                                                 @RequestParam(value = "url", required = false) String url,
                                                  @RequestParam("rewrite") Boolean rewrite) {
         // get documents storage path
         String documentStoragePath = annotationService.getAnnotationConfiguration().getFilesDirectory();
