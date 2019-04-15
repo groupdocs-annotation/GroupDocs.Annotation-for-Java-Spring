129e07408f78bc8d638c362e38aa676b07a2f0d5
koleslena <koleslena@yandex.ru>
2019-04-15 18:29:27 +0600

    Issue #47 | Added font color font size property for text annotations (#53)

--- src/main/java/com/groupdocs/ui/annotation/annotator/TextFieldAnnotator.java
+++ src/main/java/com/groupdocs/ui/annotation/annotator/TextFieldAnnotator.java
@@ -33,7 +33,6 @@ public class TextFieldAnnotator extends AbstractTextAnnotator {
         // Text field annotation
         AnnotationInfo textFieldAnnotation = initAnnotationInfo();
         textFieldAnnotation.setAnnotationPosition(new Point(annotationData.getLeft(), annotationData.getTop()));
-        textFieldAnnotation.setFontColor(255);
         textFieldAnnotation.setBackgroundColor(16777215);
         return textFieldAnnotation;
     }
@@ -54,7 +53,6 @@ public class TextFieldAnnotator extends AbstractTextAnnotator {
     public AnnotationInfo annotateImage() throws ParseException {
         // init possible types of annotations
         AnnotationInfo textFieldAnnotation = initAnnotationInfo();
-        textFieldAnnotation.setFontColor(-15988609);
         return textFieldAnnotation;
     }
 
@@ -62,7 +60,6 @@ public class TextFieldAnnotator extends AbstractTextAnnotator {
     public AnnotationInfo annotateDiagram() throws ParseException {
         // init possible types of annotations
         AnnotationInfo textFieldAnnotation = initAnnotationInfo();
-        textFieldAnnotation.setFontColor(-15988609);
         return textFieldAnnotation;
     }
 
